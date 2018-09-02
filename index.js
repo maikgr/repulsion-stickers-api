6e25f8a1a428b8f8091ac5859bf04b2f281f46dc
MGibran <maikgibranr@gmail.com>
2018-09-02 10:48:57 +0700

    Add request header setting

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ const stickerSchema = new Schema({
 });
 const Sticker = mongoose.model('stickers', stickerSchema);
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/all', (req, res) => {
     Sticker.find().select({ "_id": 0, "__v": 0 }).exec((err, stickers) => {
         if (err) console.error(err);
