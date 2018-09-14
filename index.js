c405f9166fcd37d3630a298e394c6b0f035999ff
MGibran <maikgibranr@gmail.com>
2018-09-14 14:51:34 +0700

    Add cors configuration

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const app = express();
 const Schema = mongoose.Schema;
 const db = mongoose.connection;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+const sitelist = ['https://repulsion-stickers-site.herokuapp.com, http://localhost:8080']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (sitelist.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS.'));
+        }
+    }
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+});
 db.on('error', console.error.bind(console, 'Db service connection error: '));
 db.on('connected', () => console.log('Connected to db service.'));
 db.on('disconnected', () => console.log('Disconnected from db service.'));
 
 const stickerSchema = new Schema({
     keyword: String,
-    url: String
+    url: String,
+    upload: {
+        id: String,
+        username: String,
+        date: Date
+    },
+    useCount: Number
 });
 const Sticker = mongoose.model('stickers', stickerSchema);
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://repulsion-stickers-site.herokuapp.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.get('/all', (req, res) => {
-    Sticker.find().select({ "_id": 0, "__v": 0 }).exec((err, stickers) => {
+app.get('/all', cors(corsOptions), (req, res) => {
+    Sticker.find().select({
+        "_id": 0,
+        "__v": 0
+    }).exec((err, stickers) => {
         if (err) console.error(err);
         stickers.sort(compareSticker);
         res.send(stickers);
