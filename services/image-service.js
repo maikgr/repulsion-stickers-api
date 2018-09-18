eaefcdf6a0e3d18f526a9d3d6cdf864fd92dc012
MGibran <maikgibranr@gmail.com>
2018-09-18 14:03:10 +0700

    Add image service to reupload image

--- services/image-service.js
+++ services/image-service.js
@@ -1,3 +1,19 @@
-module.exports.upload = function (url) {
-    return undefined;
+const imgur = require('imgur');
+
+const albumId = 'V7qYbqX';
+
+imgur.setCredentials(process.env.IMGUR_ID, process.env.IMGUR_PASSWORD, process.env.IMGUR_CLIENTID);
+
+module.exports.upload = async function (url) {
+    try {
+        const json = await imgur.uploadFile(url, albumId);
+
+        if (json.success) {
+            return json.data.link;
+        }
+
+        return undefined;
+    } catch (error) {
+        return undefined;
+    }
 }
