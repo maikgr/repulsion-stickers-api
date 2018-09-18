1f09c9bd6aaa0890cd402a905c8afa9ba99a3b3f
MGibran <maikgibranr@gmail.com>
2018-09-18 14:04:24 +0700

    Modify response result structure to use functions

---
+++ models/response-result.js
@@ -0,0 +1,25 @@
+"use strict"
+const moment = require('moment');
+
+module.exports.okResult = function (data) {
+    return {
+        kind: 'VariableZ#stickers',
+        code: 200,
+        requestedAt: moment().format(),
+        data: data
+    }
+}
+
+module.exports.badRequest = function (message) {
+    return {
+        code: 400,
+        message: message
+    }
+}
+
+module.exports.notFound = function (resourceKey) {
+    return {
+        code: 404,
+        message: `There's no resource found for key ${resourceKey}.`
+    }
+}
